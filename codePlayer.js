c5b1e5a9148a6f53555da408276730df6998b714
Felipe Sena <felipedosssena@gmail.com>
2024-05-28 15:01:11 -0300

    Começo do Layout

--- codePlayer.js
+++ codePlayer.js
@@ -1,3 +1,11 @@
+var editorHTML = ace.edit("editorHTML");
+var editorCSS = ace.edit("editorCSS");
 var editorJS = ace.edit("editorJS");
-    editorJS.setTheme("ace/theme/monokai");
-    editorJS.session.setMode("ace/mode/javascript");
+
+editorHTML.setTheme("ace/theme/monokai");
+editorCSS.setTheme("ace/theme/monokai");
+editorJS.setTheme("ace/theme/monokai");
+
+editorHTML.session.setMode("ace/mode/html");
+editorCSS.session.setMode("ace/mode/css");
+editorJS.session.setMode("ace/mode/javascript");
