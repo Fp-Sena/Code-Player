2dbeca20cc063718788366552e47cd7d7e8fbf46
Felipe Sena <felipedosssena@gmail.com>
2024-06-18 20:01:36 -0300

    Botões fazem os itens sumirem e reaparecerem

--- InteraçãoCodePlayer.js
+++ InteraçãoCodePlayer.js
@@ -1,29 +1,63 @@
+var tela = document.getElementById('tela');
+
 var botoes = document.getElementsByClassName('botoes');
+
 var editorHTML0 = document.getElementById('editorHTML');
-var nomePasta = document.getElementsByClassName('nomePasta');
-
-botoes[0].addEventListener('click', function(){
-    editorHTML0.remove();
-    nomePasta[0].remove();
-})
-
 var editorCSS0 = document.getElementById('editorCSS');
-
-botoes[1].addEventListener('click', function(){
-    editorCSS0.remove();
-    nomePasta[1].remove();
-});
-
 var editorJS0 = document.getElementById('editorJS');
-
-botoes[2].addEventListener('click', function(){
-    editorJS0.remove();
-    nomePasta[2].remove();
-});
-
 var output = document.getElementById('output');
 
+var nomePastaHTML = document.getElementById('nomePastaHTML');
+var nomePastaCSS = document.getElementById('nomePastaCSS');
+var nomePastaJS = document.getElementById('nomePastaJS');
+var nomePastaOutput = document.getElementById("nomePastaOutput");
+
+var contHTML = 0;
+var contCSS = 0;
+var contJS = 0;
+var contOutput = 0;
+
+botoes[0].addEventListener('click', function(){
+    if(contHTML%2==0){
+        editorHTML0.remove();
+        nomePastaHTML.remove();
+    }
+    else{
+        tela.appendChild(editorHTML0);
+        tela.appendChild(nomePastaHTML);
+    }
+    contHTML++;
+})
+botoes[1].addEventListener('click', function(){
+    if(contCSS%2==0){
+        editorCSS0.remove();
+        nomePastaCSS.remove();
+    }
+    else{
+        tela.appendChild(editorCSS0);
+        tela.appendChild(nomePastaCSS);
+    }
+    contCSS++;
+});
+botoes[2].addEventListener('click', function(){
+    if(contJS%2==0){
+        editorJS0.remove();
+        nomePastaJS.remove();
+    }
+    else{
+        tela.appendChild(editorJS0);
+        tela.appendChild(nomePastaJS);
+    }
+    contJS++;
+});
 botoes[3].addEventListener('click', function(){
-    output.remove();
-    nomePasta[3].remove();
+    if(contOutput%2==0){
+        output.remove();
+        nomePastaOutput.remove();
+    }
+    else{
+        tela.appendChild(output);
+        tela.appendChild(nomePastaOutput);
+    }
+    contOutput++;
 })
